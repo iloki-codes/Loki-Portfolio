d628500560f3875effafeb31769b98a01f327e95
Lokesh <lokesh.loki.codes@gmail.com>
2025-08-11 12:18:34 +0530

    build issue fixed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,8 @@
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 import autoprefixer from 'autoprefixer';
 import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
       react(),
